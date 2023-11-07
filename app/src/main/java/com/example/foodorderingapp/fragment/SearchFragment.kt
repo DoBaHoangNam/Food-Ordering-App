01c3285d36bd5b5503026e6c6fc9470bb79b0d0b
Hoang Nam <hoangnam2412lc@gmail.com>
2023-11-07 12:25:26 +0700

    update Search function

--- app/src/main/java/com/example/foodorderingapp/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/foodorderingapp/fragment/SearchFragment.kt
@@ -1,16 +1,13 @@
 package com.example.foodorderingapp.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodorderingapp.R
 import com.example.foodorderingapp.adapter.MenuAdapter
-import com.example.foodorderingapp.adapter.PopularAdapter
-import com.example.foodorderingapp.databinding.FragmentHomeBinding
 import com.example.foodorderingapp.databinding.FragmentSearchBinding
 import com.example.foodorderingapp.model.Food
 
@@ -31,18 +28,13 @@ class SearchFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentSearchBinding.inflate(inflater,container,false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         binding.recvFoodList.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-        adapter = MenuAdapter(getListFoods())
+        adapter = MenuAdapter(filterMenuFood)
         binding.recvFoodList.adapter = adapter
         setupSearchView()
         showAllMenu()
+        return binding.root
     }
-
 
     private fun showAllMenu() {
         filterMenuFood.clear()
@@ -65,12 +57,11 @@ class SearchFragment : Fragment() {
     private fun setupSearchView() {
         binding.searchEdt.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
-
                 filterMenuItems(query)
                 return true
             }
-            override fun onQueryTextChange(newText: String): Boolean {
-                filterMenuItems(newText)
+            override fun onQueryTextChange(query: String): Boolean {
+                filterMenuItems(query)
                 return true
             }
         })
